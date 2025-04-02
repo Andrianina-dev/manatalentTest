5664f19883574ed08bfe737be4f3ce169cf9edef
andrianinaDev <andrianinarandria001@hotmail.com>
2025-04-02 20:04:03 +0300

    CHANGE

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'yamabiko.proxy.rlwy.net',
   user: process.env.DB_USER || 'root',
-  user: process.env.DB_PORT || '45015',
+  user: process.env.DB_PORT || '3000',
   password: process.env.DB_PASSWORD || 'uSrdnqCRaKAnwWIJYnEUNhLZQUrrlBgc',
   database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
