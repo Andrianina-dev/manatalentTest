d9d36341a38dc6d2e152ac61f44c03cbc495a963
andrianinaDev <andrianinarandria001@hotmail.com>
2025-04-02 16:37:07 +0300

    changemen

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'mysql.railway.internal',
   user: process.env.DB_USER || 'root',
+  user: process.env.DB_PORT || '45015',
   password: process.env.DB_PASSWORD || 'uSrdnqCRaKAnwWIJYnEUNhLZQUrrlBgc',
   database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
